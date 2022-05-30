247bea97d4112dd1adb4870e4c8840fd1a67d381
Ashutosh Hans <ashutosh.hans@gmail.com>
2022-05-30 22:18:41 +0530

    convert to class, use dynamic variable

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  // Available on all the class components, it is run first whenever this app is constructed built or instantiated
+  constructor(props) {
+    super();
+
+    // react is looking for this inside constructor
+    this.state = {
+      name: 'Ashutosh'
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Hello! {this.state.name}</p>
+          <button>Change Name</button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
