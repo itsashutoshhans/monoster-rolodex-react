390d2341b1b9f4cf73ee15de11776bc16f28aafe
Ashutosh Hans <ashutosh.hans@gmail.com>
2022-09-16 19:25:51 +0530

    create card, card-list components and finishing touches

--- src/App.js
+++ src/App.js
@@ -60,9 +60,10 @@ class App extends Component {
           placeholder="search monsters"
           onChange={onSearchChange}
         /> */}
+        <h1 className="app-title">Monsters Rolodex</h1>
         <SearchBox
           onChangeHandler={onSearchChange}
-          className="search-box"
+          className="monsters-search-box"
           placeholder="search monsters"
         />
         <CardList monsters={filteredMonsters} />
