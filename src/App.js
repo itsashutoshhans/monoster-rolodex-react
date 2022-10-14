43eea3e9236258f7a53b29e186d5bfb4b8302acb
Ashutosh Hans <ashutosh.hans@gmail.com>
2022-10-14 15:05:22 +0530

    adding hooks and converting everything to functional component

--- src/App.js
+++ src/App.js
@@ -1,75 +1,55 @@
-import { Component } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import CardList from "./components/card-list/card-list.component";
 import SearchBox from "./components/search-box/search-box.component";
 
-class App extends Component {
-  // Available on all the class components, it is run first whenever this app is constructed built or instantiated
-  constructor(props) {
-    super();
+const App = () => {
+  const [searchField, setSearchField] = useState(""); //[value, setValue]
+  const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilteredMonsters] = useState(monsters);
 
-    // react is looking for this inside constructor
-    this.state = {
-      monsters: [],
-      searchString: "",
-    };
-  }
-
-  componentDidMount() {
-    // Mounting is - the first time the component gets placed onto the DOM
-    // OR The first time react renders the component onto the page
-    // Only happens once in the component lifecycle
-    // remount only happens after unmount
-
-    // api calls are placed here
+  useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
-      .then((users) =>
-        this.setState(
-          () => {
-            return { monsters: users, filteredMonsters: users };
-          },
-          () => console.log(this.state)
-        )
-      );
-  }
+      .then((users) => {
+        setMonsters(users);
+      });
+  }, []);
 
-  onSearchChange = (event) => {
+  // comes under useEffect to prevent re creation of fileterd monsters because of other side effects and improve performance
+  useEffect(() => {
+    const newFilteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
+
+    setFilteredMonsters(newFilteredMonsters);
+  }, [monsters, searchField]);
+
+  const onSearchChange = (event) => {
     // this anonymous function is problematic because with every render it is thrown away and recreated
     // every time we are re initializing it we are making our app less perfomant
     const searchString = event.target.value.toLocaleLowerCase();
-    this.setState(() => {
-      return { searchString };
-    });
+    setSearchField(searchString);
   };
 
-  render() {
-    const { monsters, searchString } = this.state;
-    const { onSearchChange } = this;
-    // destructuring - makes it readable
-    const filteredMonsters = monsters.filter((monster) => {
-      return monster.name.toLocaleLowerCase().includes(searchString);
-    });
-
-    return (
-      <div className="App">
-        {/* input looks like the html but it is a react component inside, because react allows us to bind functionality with the UI - also same goes with html properties or attributes */}
-        {/* <input
+  return (
+    <div className="App">
+      {/* input looks like the html but it is a react component inside, because react allows us to bind functionality with the UI - also same goes with html properties or attributes */}
+      {/* <input
           className="search-box"
           type="search"
           placeholder="search monsters"
           onChange={onSearchChange}
         /> */}
-        <h1 className="app-title">Monsters Rolodex</h1>
-        <SearchBox
-          onChangeHandler={onSearchChange}
-          className="monsters-search-box"
-          placeholder="search monsters"
-        />
-        <CardList monsters={filteredMonsters} />
-      </div>
-    );
-  }
-}
+      <h1 className="app-title">Monsters Rolodex</h1>
+      <SearchBox
+        onChangeHandler={onSearchChange}
+        className="monsters-search-box"
+        placeholder="search monsters"
+      />
+      <CardList monsters={filteredMonsters} />
+    </div>
+  );
+};
 
 export default App;
